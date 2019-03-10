29b1f296889eeae851ac4d356fd85a7d65cd18d1
Alexander Rowe <alex.rowe@senseye.co>
2019-03-09 22:54:59 -0600

    Added Long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='pyvent',
     version='0.1.0',
     description='IPC over ZMQ, made easy',
+    long_description=long_description,
     author='Alexander Rowe',
     author_email='alexrowe707@gmail.com',
     python_requires='>=3.6.0',
