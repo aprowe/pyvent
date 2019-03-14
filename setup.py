0cb989d37e0260eedf367bcb316cdd9fe793bc7b
Alexander Rowe <alex.rowe@senseye.co>
2019-03-13 23:10:44 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='pyvent',
-    version='0.1.1',
+    version='0.1.2',
     description='IPC over ZMQ, made easy',
     long_description=long_description,
     author='Alexander Rowe',
